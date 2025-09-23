4a5e223cb2eb717aabccce4a7d87372b472ebe6a
Reclax <marcoserrano2206@gmail.com>
2025-09-23 10:17:16 -0500

    Se crea la implementacion de los servicios y controladores del microservicio autenticacion

---
+++ Consultorio_Medico_Backend/Consulltorio_Medico_Autenticacion/Controllers/UsuariosController.cs
@@ -0,0 +1,46 @@
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using Consulltorio_Medico_Autenticacion.Auth;
+using Consulltorio_Medico_Administracion.Protos;
+
+namespace Consulltorio_Medico_Autenticacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public UsuariosController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // POST: api/Usuarios
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<String>> PostUsuario(UsuarioLogin usuario)
+        {
+
+            using var canal = GrpcChannel.ForAddress(_config["grcp:administracion"]);
+
+            var cliente = new UsuarioService.UsuarioServiceClient(canal);
+
+            var respuesta = await cliente.ValidarUsuarioAsync(new UsuarioLogin
+            {
+                NombreUsuario = usuario.NombreUsuario,
+                Contrasenia = usuario.Contrasenia
+            });
+
+            if (!respuesta.EsValido && respuesta.Usuario == null)
+            {
+                return BadRequest();
+            }
+            var TokenProvider = new TokenProvider(_config);
+
+            var Token = TokenProvider.Create(respuesta.Usuario);
+            return Token;
+        }
+    }
+}
+

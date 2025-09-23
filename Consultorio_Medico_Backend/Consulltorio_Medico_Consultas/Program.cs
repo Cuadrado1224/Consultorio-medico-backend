16c783d1febb7fcd75966c8efea937a2664191bf
materubag <mrubio7466@uta.edu.ec>
2025-09-23 16:36:30 -0500

    se agregan controllers y archivos Docker

--- Consultorio_Medico_Backend/Consulltorio_Medico_Consultas/Program.cs
+++ Consultorio_Medico_Backend/Consulltorio_Medico_Consultas/Program.cs
@@ -1,19 +1,97 @@
-var builder = WebApplication.CreateBuilder(args);
 
+using Microsoft.EntityFrameworkCore;
+
+using Consulltorio_Medico_Consultas.Data;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Consulltorio_Medico_Consultas.protos;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+string? connectionString = builder.Configuration.GetConnectionString("HospitalConnection");
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 // Add services to the container.
+
+
+builder.Services.AddGrpc();
+//dotnet dev-certs https --trust
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(7230, listenOptions =>
+    {
+        listenOptions.UseHttps(); // ✅ Usa el certificado por defecto o personalizado
+        listenOptions.Protocols = HttpProtocols.Http2;
+    });
+});
+
+
+//migracion
+
+
+
+//JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
+{
+    o.Events = new JwtBearerEvents
+    {
+        OnAuthenticationFailed = context =>
+        {
+            Console.WriteLine($"Error de autenticación: {context.Exception}");
+            return Task.CompletedTask;
+        },
+        OnForbidden = context =>
+        {
+            Console.WriteLine($"Error de autenticación: {context.Response}");
+            return Task.CompletedTask;
+        },
+        OnTokenValidated = context =>
+        {
+            Console.WriteLine($"Token validado: {context.SecurityToken}");
+            return Task.CompletedTask;
+        }
+    };
+
+    o.RequireHttpsMetadata = false;
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]!)),
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+
+        ClockSkew = TimeSpan.Zero
+    };
+
+});
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+    db.Database.Migrate(); // <-- esto aplica las migraciones
+}
+
+app.MapGrpcService<PacienteServiceImpl>();
+app.MapGrpcService<ConsultasServiceImpl>();
+
+app.MapGet("/", () => "Comunicacion a trav�s de GRPC");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    _ = app.UseSwagger();
+    _ = app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();

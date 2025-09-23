ddad7ebf7b236c9f2162268fac52ee2681def2ba
Reclax <marcoserrano2206@gmail.com>
2025-09-23 10:27:27 -0500

    Se corrige el archivo de arranque

--- Consultorio_Medico_Backend/Consulltorio_Medico_Autenticacion/Program.cs
+++ Consultorio_Medico_Backend/Consulltorio_Medico_Autenticacion/Program.cs
@@ -4,27 +4,23 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddGrpc();
-//dotnet dev-certs https --trust
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(7148, listenOptions =>
     {
-        listenOptions.UseHttps(); // ✅ Usa el certificado por defecto o personalizado
+        listenOptions.UseHttps();
         listenOptions.Protocols = HttpProtocols.Http2;
     });
 });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
